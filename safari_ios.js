c36a42a094678af8b2a0893f6413a73787bcf8ad
Daniel Myers <jdanielmyers@gmail.com>
2022-11-23 10:05:12 -0600

    Create safari_ios.js

---
+++ safari_ios.js
@@ -0,0 +1,15 @@
+/**
+ * Utility function to add CSS in multiple passes.
+ * @param {string} styleString
+ */
+function addStyle(styleString) {
+  const style = document.createElement('style');
+  style.textContent = styleString;
+  document.head.append(style);
+}
+
+addStyle(`
+  html, body {
+    min-width: 1024px;
+  }
+`);
